689893e834ddc39decd4dd18a083969b27eb5dc2
César Donato <cesarxavier222@gmail.com>
2024-08-07 11:51:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-
+#Declarando uma variavel Janelas com o Tkinter
 janela=tk.Tk()
 janela.title('Cotação Moeda')
-
+#Configurando as linhas e colunas da janela
 janela.rowconfigure(0,weight=1)
 janela.columnconfigure([0,1],weight=1)
-
+#Inserindo o primeiro labe, declarando sua cor de fundo e das letras, e tamanho
 mensagem=tk.Label(text='Sistema de Busca de Cotação de Moedas',fg='white',bg='black',width=35,height=5)
 mensagem.grid(row=0,column=0,columnspan=2,sticky='NSEW')
 
